e8ed9534666d7dff9168c0c403fe37b98dff7431
Matt Insler <matt.insler@gmail.com>
2013-04-20 22:06:29 -0700

    changed example url

--- example/server.js
+++ example/server.js
@@ -9,7 +9,7 @@ app.use(express.cookieParser());
 app.use(express.session({secret: 'irhe082h304ufhqu9eyft9eg'}));
 app.use(app.router);
 
-var keyless = Keyless({server: 'http://keyless.unifiedsocial.dev', shared_key: 'fafafeefee'});
+var keyless = Keyless({server: 'http://keyless.dev', shared_key: 'fafafeefee'});
 
 app.get('/', keyless.protect, function(req, res) {
   var user = req.keyless_user;
