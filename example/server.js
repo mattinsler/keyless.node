b95a70ac4076111aa7f3a0007204dc0336f490b9
Matt Insler <matt.insler@gmail.com>
2013-05-10 14:14:38 -0700

    added middleware to example

--- example/server.js
+++ example/server.js
@@ -1,5 +1,6 @@
 var express = require('express')
-  , Keyless = require('keyless.node');
+  , Keyless = require('keyless.node')
+  , keyless = Keyless({server: 'http://keyless.unified.dev', shared_key: 'fafafeefee', authorization_data: {service: 'foobar'}});
 
 var app = express();
 
@@ -7,9 +8,8 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser());
 app.use(express.session({secret: 'irhe082h304ufhqu9eyft9eg'}));
+app.use(keyless.middleware())
 app.use(app.router);
-
-var keyless = Keyless({server: 'http://keyless.unified.dev', shared_key: 'fafafeefee', authorization_data: {service: 'foobar'}});
 
 app.get('/', keyless.protect, function(req, res) {
   res.end(JSON.stringify(req.keyless_user, null, 2));
